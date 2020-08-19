80f8e332cb7825c77361c69d276c3b10ead10809
Flargy <marlun1996@hotmail.com>
2020-08-19 11:09:53 +0200

    first patch
    base functionality added

---
+++ SpaceShooter/Assets/Scripts/GameMaster/GameVariables.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameVariables : MonoBehaviour
+{
+    static public float GameTime { get; set; }
+    static public Transform PlayerTransform { get; set; }
+
+    static private List<GameObject> enemies = new List<GameObject>();
+
+    static private List<GameObject> projectiles = new List<GameObject>();
+
+    private void Update()
+    {
+        GameTime = Time.deltaTime;
+    }
+
+    private void RegisterEnemy(GameObject enemy)
+    {
+        enemies.Add(enemy);
+    }
+
+    private void RemoveEnemy(GameObject enemy)
+    {
+        if (enemies.Contains(enemy))
+        {
+            enemies.Remove(enemy);
+        }
+    }
+
+    private void EmptyList(ref List<GameObject> list)
+    {
+        foreach(GameObject obj in list)
+        {
+            Destroy(obj, 0.1f);
+        }
+        list.Clear();
+    }
+
+    private void RegisterProjectile(GameObject projectile)
+    {
+        projectiles.Add(projectile);
+    }
+
+    private void RemoveProjectile(GameObject projectile)
+    {
+        if (projectiles.Contains(projectile))
+        {
+            projectiles.Remove(projectile);
+        }
+    }
+}
---
+++ SpaceShooter/Assets/Scripts/Player/PlayerBehaviour.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed = 5f;
+
+    private int baseDamage = 1;
+    private float currentDamage = 0f;
+    private Vector3 direction = Vector3.zero;
+    private float currentFireRate = 0f;
+    private int fireRate = 1;
+    private float cooldownTimer = 0f;
+    
+
+
+    void Awake()
+    {
+        currentDamage = baseDamage;
+        currentFireRate = fireRate;
+    }
+
+    void Update()
+    {
+        direction.x = Input.GetAxis("Horizontal");
+        direction.y = Input.GetAxis("Vertical");
+        cooldownTimer += GameVariables.GameTime;
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Fire();
+        }
+        Move();
+        GameVariables.PlayerTransform = transform;
+    }
+
+    private void Fire()
+    {
+        //cooldownTimer >= currentFireRate ? Debug.Log("hej") : Debug.Log("inte hej");
+        if(cooldownTimer >= currentFireRate)
+        {
+            Debug.Log("fire");
+            cooldownTimer = 0;
+        }
+        else
+        {
+            Debug.Log("not fire");
+        }
+    }
+
+    private void Move()
+    {
+
+        transform.position += direction * movementSpeed * GameVariables.GameTime;
+
+    }
+
+    
+}
